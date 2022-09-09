ff62808c78305b5b1bfb77ab516d089d185e9eb6
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-09-09 10:29:31 +0200

    fetching meals data

--- React-practice-food/src/components/Meals/AvailableMeals.js
+++ React-practice-food/src/components/Meals/AvailableMeals.js
@@ -7,10 +7,15 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(()=> {
     const fetchMeals = async () => {
       const response = await fetch('https://react-http-6b4a6.firebaseio.com/meals.json');
+
+      if (!response.ok) {
+        throw new Error ("Something went wrong!");
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -26,16 +31,28 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     }
+    
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
 
-    fetchMeals();
   }, []);
 
   if (isLoading) {
     return (
-      <section>
+      <section className={classes.MealsLoading}>
       <p>Loading...</p>
       </section>
     );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
   }
 
   const mealsList = meals.map((meal) => (
