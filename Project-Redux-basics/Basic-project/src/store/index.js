b27b85073d86aa7080995eefe2330d1b47d4152b
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-09-12 13:18:25 +0200

    dispatching actions and attaching payloads to actions

--- Project-Redux-basics/Basic-project/src/store/index.js
+++ Project-Redux-basics/Basic-project/src/store/index.js
@@ -1,23 +1,27 @@
-import { legacy_createStore } from 'redux';
+import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === 'increment') {
     return {
-      couter: state.counter + 1,
+      counter: state.counter + 1,
     };
   }
+
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount,
+    };
+  }
+
   if (action.type === 'decrement') {
     return {
-      couter: state.counter - 1,
+      counter: state.counter - 1,
     };
   }
 
   return state;
 };
 
-const store = legacy_createStore(counterReducer);
-
-store.dispatch('increment');
-store.dispatch('decrement');
+const store = createStore(counterReducer);
 
 export default store;
