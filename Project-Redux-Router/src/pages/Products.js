d034e19979445e7090b36931ffe405b200536e8c
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-09-17 12:19:51 +0200

    finish

--- Project-Redux-Router/src/pages/Products.js
+++ Project-Redux-Router/src/pages/Products.js
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
-    <h1>The Products Page</h1>
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>
+          <Link to='/products/p1'>Book</Link>
+        </li>
+        <li>
+          <Link to='/products/p2'>Carpet</Link>
+        </li>
+        <li>
+          <Link to='/products/p3'>Online Course</Link>
+        </li>
+      </ul>
+    </section>
   )
 };
 
