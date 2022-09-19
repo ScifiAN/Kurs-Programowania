12aa3951ff9557c0d76913474de801eee84f9e9a
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-09-19 09:57:26 +0200

    programmatic navigation

--- Project-Redux-Router-Practice/src/pages/NewQuote.js
+++ Project-Redux-Router-Practice/src/pages/NewQuote.js
@@ -1,7 +1,13 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
-  const addQuoteHandler = (quoteData) => {};
+  const history = useHistory();
+
+  const addQuoteHandler = (quoteData) => {
+    history.push('/quotes');
+  };
 
   return (
     <QuoteForm onAddQuote={addQuoteHandler} />
