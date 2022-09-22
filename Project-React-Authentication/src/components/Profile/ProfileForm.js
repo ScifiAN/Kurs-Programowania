9e9348c3548cd8ff4595e6f230d1f8679d50d56f
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-09-22 14:38:12 +0200

    redirecting user and adding logout

--- Project-React-Authentication/src/components/Profile/ProfileForm.js
+++ Project-React-Authentication/src/components/Profile/ProfileForm.js
@@ -1,9 +1,11 @@
 import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import classes from './ProfileForm.module.css';
 import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+  const history = useHistory();
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -25,6 +27,7 @@ const ProfileForm = () => {
       }
     }).then(res => {
       //assumption: success
+      history.replace('/');
     })
   };
 
