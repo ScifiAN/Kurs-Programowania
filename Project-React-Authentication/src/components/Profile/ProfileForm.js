dd9e2bfab1f63536b2b8c0198cda1b299f13bd1c
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-09-22 14:29:35 +0200

    managing auth state and using token

--- Project-React-Authentication/src/components/Profile/ProfileForm.js
+++ Project-React-Authentication/src/components/Profile/ProfileForm.js
@@ -1,11 +1,36 @@
+import { useRef, useContext } from 'react';
+
 import classes from './ProfileForm.module.css';
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enterdNewPassword = newPasswordInputRef.current.value;
+    //add validation
+
+    fetch('https://dummybackend.com/accounts:update?key=dummykey84756', {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enterdNewPassword,
+        returnSecureToken: false,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength='7' ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
