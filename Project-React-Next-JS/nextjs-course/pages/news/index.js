94f5da28f9644539ab558d95a24db9e0e037a0e4
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-09-24 12:28:29 +0200

    tutorial finished

--- Project-React-Next-JS/nextjs-course/pages/news/index.js
+++ Project-React-Next-JS/nextjs-course/pages/news/index.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 const NewsPage = () => {
   return (
-    <h1>The News Page</h1>
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs">NextJs</Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
   );
-}
+};
 
 export default NewsPage;
