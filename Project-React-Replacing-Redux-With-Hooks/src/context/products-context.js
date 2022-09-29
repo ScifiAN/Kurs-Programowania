35150db1221948fc5b957ddf742f09deecd33628
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-09-29 15:22:12 +0200

    using context api

--- Project-React-Replacing-Redux-With-Hooks/src/context/products-context.js
+++ Project-React-Replacing-Redux-With-Hooks/src/context/products-context.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ProductsContext = React.createContext({
-  products: [
+export const ProductsContext = React.createContext({
+  products: [],
+  toggleFav: (id) => {},
+});
+
+export default (props) => {
+  const [productsList, setProductsList] = useState([
     {
       id: "p1",
       title: "Red Scarf",
@@ -26,7 +31,26 @@ const ProductsContext = React.createContext({
       description: "Street style! An orange hat.",
       isFavorite: false,
     },
-  ],
-});
+  ]);
 
-export default ProductsContext;
+  const toggleFavorite = (productId) => {
+    setProductsList((currentProdList) => {
+      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+      const newFavStatus = !currentProdList[prodIndex].isFavorite;
+      const updatedProducts = [...currentProdList];
+      updatedProducts[prodIndex] = {
+        ...currentProdList[prodIndex],
+        isFavorite: newFavStatus,
+      };
+      return updatedProducts;
+    });
+  };
+
+  return (
+    <ProductsContext.Provider
+      value={{ products: productsList, toggleFav: toggleFavorite }}
+    >
+      {props.children}
+    </ProductsContext.Provider>
+  );
+};
