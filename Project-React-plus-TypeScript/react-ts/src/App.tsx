d015654607e3433130e5dfef73f6227db74da136
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-10-04 10:15:43 +0200

    adding todo

--- Project-React-plus-TypeScript/react-ts/src/App.tsx
+++ Project-React-plus-TypeScript/react-ts/src/App.tsx
@@ -1,10 +1,28 @@
-import Todos from './components/Todo';
-import './App.css';
+import { useState } from "react";
+
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  // const todos = [
+  //   new Todo('Learn React'),
+  //   new Todo('Learn Typescript'),
+  // ];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
   return (
     <div>
-      <Todos />
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
     </div>
   );
 }
